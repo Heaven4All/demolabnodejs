48c0384991df92e9e4d76260d4600c2e4b664914
TABARLY Sébastien <sebastien.tabarly@sogeti.com>
2021-09-07 14:33:00 +0200

    Fix command injection

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const https = require('https')
 const bodyParser = require('body-parser')
 const exec = require("child_process").exec;
 require('dotenv').config();
+// Fixit 
+const fs = require('fs');
+
 
 const app = express()
 
@@ -25,9 +28,14 @@ app.post('/', (req, res) => {
   const appId = process.env.APIKEY;
   const url = "https://api.openweathermap.org/data/2.5/weather?q=" + location + "&APPID=" + appId + "&units=metric";
 
-  exec(`echo ${location} >> log.txt`, function (error, stdout, stderr) {
-    console.dir(stdout);
-  })
+  //exec(`echo ${location} >> log.txt`, function (error, stdout, stderr) {
+  //  console.dir(stdout);
+  //})
+  
+  fs.writeFile('logs.txt', location, function (err) {
+	  if (err) return console.log(err);
+	  console.log('New city search logged');
+  });
 
 
   https.get(url, (response) => {
